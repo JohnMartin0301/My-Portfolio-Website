1e366db12ed2a8a48a80cf395e1225e52c7c29e0
j_martin <112761826+JohnMartin0301@users.noreply.github.com>
2023-08-05 15:36:45 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			fill="none"
-			viewBox="0 0 16 16"
+			viewBox="0 0 24 24"
 			strokeWidth={1.5}
 			stroke="currentColor"
 			className="w-6 h-6"
