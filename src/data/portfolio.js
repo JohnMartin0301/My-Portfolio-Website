8343874fec81ab9d095079d9176024f4f96e1363
j_martin <112761826+JohnMartin0301@users.noreply.github.com>
2023-08-05 15:19:38 +0800

    Update portfolio.js

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
     {
         title: 'Personal Finance Tracker',
-        imgUrl: '/assets/pft0.png',
+        imgUrl: '/public/assets/pft0.png',
         stack: ['Java'],
         link: 'https://github.com/JohnMartin0301/Personal-Finance-Tracker-Java-',
     },
     {
         title: 'SecURL',
-        imgUrl: '/assets/SecURL1.png',
+        imgUrl: '/public/assets/SecURL1.png',
         stack: ['Python'],
         link: 'https://github.com/JohnMartin0301/SecURL',
     },
     {
         title: 'Visual|Sortify',
-        imgUrl: '/assets/vs.png',
+        imgUrl: '/public/assets/vs.png',
         stack: ['HTML5', 'CSS3', 'JavaScript'],
         link: 'https://github.com/JohnMartin0301/Visual-Sortify',
     },
