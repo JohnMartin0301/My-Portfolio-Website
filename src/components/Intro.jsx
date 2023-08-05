3a4459ae0cad9c0fd41e7131089ee986b4c94b74
John Martin <johnmartin0301@gmail.com>
2023-08-05 14:09:24 +0800

    add: initial files

---
+++ src/components/Intro.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Intro () {
+    return (
+        <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+            <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">j_martin💻</h1>
+            <p className="text-base md:text-xl mb-3 font-medium">"Someone Born to Code"</p>
+            <p className="text-sm max-w-xl mb-6 font-bold">
+                I am John Martin, and I have acquired a strong foundation in programming languages 
+                such as Python, Java and JavaScript. My experience includes working on various 
+                projects, including web development, web-based applications, and GUI apps.
+                As a hobbyist programmer, I am deeply passionate about utilizing technology to solve 
+                problems and build efficient systems.  I am always eager to learn new technologies and 
+                programming languages, and I am dedicated to continuous growth in the field of software 
+                engineering.
+            </p>
+        </div>
+    )
+}
+
+export default Intro;
---
+++ src/data/timeline.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        year: '2019',
+        title: 'First Line of Code',
+        duration: 'the start',
+        details: 
+        'During my exciting journey into coding, I discovered the captivating world of programming when I was introduced to Java in a programming class.  Writing my first lines of Java code brought a mix of excitement and nervousness as I witnessed the computer responding to my commands, breathing life into the machine. Delving deeper, I became captivated by the versatility and power of Java, learning about variables, loops, and object-oriented programming. ',
+    },
+    {
+        year: '2020',
+        title: 'Discovering Python',
+        duration: '1 year',
+        details: "After immersing myself in Java programming for a year, I discovered Python, which completely transformed my perspective. Python's simplicity and readability immediately captured my attention, like uncovering a hidden treasure of elegant code. Exploring Python's features drew me deeper into the programming world.",
+    },
+    {
+        year: '2021',
+        title: 'Web-bound',
+        duration: '1 year',
+        details: "As my programming journey progressed, I felt compelled to explore new realms beyond GUI-based programming. This led me to venture into the world of web development, where I discovered the transformative power of HTML and CSS. Through these technologies, I realized the potential to create visually captivating and interactive web experiences, breathing life into static content.",
+    },
+    {
+        year: 'Today',
+        title: 'Evolving',
+        duration: '2 years',
+        details: "Dedicated to mastering HTML, CSS, JavaScript, and React.js, I delved into personal projects and worked part-time during summer breaks. This exploration became a pivotal moment in my software engineering journey, combining my programming passion with the creation of visually captivating web experiences.",
+    },
+]
